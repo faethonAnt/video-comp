06bb02621b9bdac395cb7488ae1d1c595519eef7
FAETHON THEODOROS ANTONOPOULOS <antonopfaeth@gmail.com>
2026-04-04 18:38:31 +0300

    video upload now needs to be under 20sec

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,9 @@
 const multer = require("multer");
 const db = require("../db/database");
 
+// * this is used to see video metadata so we can check video duration
+const ffmpeg = require("fluent-ffmpeg");
+
 // ? imporrt destructuring because auth middleware exports two functions
 const { isUser } = require("../middleware/authMiddleware");
 const express = require("express");
@@ -18,31 +21,45 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-// ! router.post("/upload", upload.single("video"), (req, res) => { this is used for testing purposes only
+router.post("/upload", upload.single("video"), (req, res) => {
+  //this is used for testing purposes only
 
-router.post("/upload", isUser, upload.single("video"), (req, res) => {
+  //router.post("/upload", isUser, upload.single("video"), (req, res) => {
   const title = req.body.title;
-  // ! const userId = 1; this is used for testing purposes only
-  const userId = req.user.id;
+  const userId = 1; //this is used for testing purposes only
+  //const userId = req.user.id;
   const filePath = req.file.path;
 
-  // USER SEARCH
-  db.get("SELECT * FROM video WHERE userId = ?", [userId], (err, user) => {
-    // Database fail
+  ffmpeg.ffprobe(filePath, (err, metadata) => {
+    const duration = metadata.format.duration;
     if (err) res.status(500).json({ message: "Something went wrong" });
-    //user already in the system
-    if (user) res.status(401).json({ message: "unauthorized" });
-    //new user
-    else {
-      db.run(
-        "INSERT INTO video (title, userId, status, filePath) VALUES (?, ?, ?, ?)",
-        [title, userId, "pending", filePath],
-        (err) => {
-          if (err)
-            return res.status(500).json({ message: "Something went wrong" });
-          res.status(200).json({ message: "Success" });
-        },
-      );
+    if (duration > 20) {
+      res.status(400).json({ message: "Video must not excede 20 seconds" });
+    } else {
+      // USER SEARCH
+      db.get("SELECT * FROM video WHERE userId = ?", [userId], (err, user) => {
+        // Database fail
+        if (err) res.status(500).json({ message: "Something went wrong" });
+        //user already in the system
+        if (user)
+          res
+            .status(401)
+            .json({ message: "You have already uploaded a video" });
+        //new user
+        else {
+          db.run(
+            "INSERT INTO video (title, userId, status, filePath) VALUES (?, ?, ?, ?)",
+            [title, userId, "pending", filePath],
+            (err) => {
+              if (err)
+                return res
+                  .status(500)
+                  .json({ message: "Something went wrong" });
+              res.status(200).json({ message: "Success" });
+            },
+          );
+        }
+      });
     }
   });
 });
