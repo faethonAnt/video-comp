6cb0d69ccaee34e7c31fc272a042940d8eab2f75
FAETHON THEODOROS ANTONOPOULOS <antonopfaeth@gmail.com>
2026-04-06 07:59:53 +0300

    admin video list & status acception/rejection

--- routes/admin.js
+++ routes/admin.js
@@ -35,9 +35,43 @@ router.post("/login", (req, res) => {
 // logout
 router.get("/logout", isAdmin, (req, res) => {
   req.session.destroy((err) => {
-    if (err) console.log("didnt destroy");
+    if (err) return console.log("didnt destroy");
     else res.status(200).json({ message: "Success!" });
   });
+});
+
+//list of videos
+router.get("/videos", isAdmin, (req, res) => {
+  db.all("SELECT * FROM video", (err, videos) => {
+    if (err) return res.status(500).json({ message: "Server response" });
+    res.status(200).json({ videos }); //sending the array back as JSON the front end will handle it later
+  });
+});
+
+router.put("/videos/:id/approve", isAdmin, (req, res) => {
+  //:id extracts the video id from the URL
+  const id = req.params.id;
+
+  db.run(
+    "UPDATE video SET status = ? WHERE id = ?",
+    ["approved", id],
+    (err) => {
+      if (err) return res.status(500).json({ message: "Something went wrong" });
+      res.status(200).json({ message: "Video approved" });
+    },
+  );
+});
+
+router.put("/videos/:id/reject", isAdmin, (req, res) => {
+  const id = req.params.id;
+  db.run(
+    "UPDATE video SET status = ? WHERE id = ?",
+    ["rejected", id],
+    (err) => {
+      if (err) return res.status(500).json({ message: "Something went wrong" });
+      res.status(200).json({ message: "Video rejected" });
+    },
+  );
 });
 
 module.exports = router;
